8079c54ed36f0cb3d714aa0d5b1646f45984bfe9
AlexLMCode <alexlm0212@gmail.com>
2020-08-08 13:24:14 -0500

    Added the docs directory

---
+++ docs/precache-manifest.ff6deebc594603fd9e7163dd313d6f71.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87e6d3f972a587ae8c35e56bc715bd99",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1441705bf2099bc45ac4",
+    "url": "/static/css/main.41fd933d.chunk.css"
+  },
+  {
+    "revision": "3ec50c8fead662706b17",
+    "url": "/static/js/2.637c77a8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.637c77a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1441705bf2099bc45ac4",
+    "url": "/static/js/main.e1d0eeaf.chunk.js"
+  },
+  {
+    "revision": "2beaf3c9299542d4240e",
+    "url": "/static/js/runtime-main.c8ba4116.js"
+  }
+]);
