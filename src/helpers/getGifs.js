c33deb76bf3eb92f4180c9b0d91cdbf864be1b34
AlexLMCode <alexlm0212@gmail.com>
2020-08-08 13:39:54 -0500

    Added the https server

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,6 +1,6 @@
 
 const getGifs = async (category) => {
-    const url = `http://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&api_key=P57JjpAyJblb0CFBgFvVlC7rRH9TGugQ&limit=10`
+    const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&api_key=P57JjpAyJblb0CFBgFvVlC7rRH9TGugQ&limit=10`
     const respuesta = await fetch(url);
     const {data} = await respuesta.json();
 
